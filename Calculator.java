37cb24ff5b62f349887dd850cd47f04d464e54cf
Priyanka <pvhatkar128@gmail.com>
2026-03-25 13:06:54 +0530

    calculator basic

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+// Calculator.java
+public class Calculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Cannot divide by zero");
+            return 0;
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + add(a, b));
+        System.out.println("Subtraction: " + subtract(a, b));
+        System.out.println("Multiplication: " + multiply(a, b));
+        System.out.println("Division: " + divide(a, b));
+    }
+}
